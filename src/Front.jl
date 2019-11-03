46edc5885379a94edf642322861d8c9750f44ad1
Alexandr Romanenko <waralex@gmail.com>
2019-11-03 16:13:24 +0300

    fix

--- src/Front.jl
+++ src/Front.jl
@@ -1,14 +1,17 @@
 module Front
-import JSON, PlotlyBase
+import JSON, JSON2, PlotlyBase
 
 
 to_dash(t::Any) = t
 from_dash(::Type{Any}, t::Any) = t
 
-to_dash(t::PlotlyBase.Plot) = JSON.lower(t)
+
 function from_dash(::Type{PlotlyBase.Plot}, t) 
     data = PlotlyBase.GenericTrace[PlotlyBase.GenericTrace(tr) for tr in t.data]
     layout = PlotlyBase.Layout(t.layout)
     PlotlyBase.Plot(data, layout)
 end
+
+#FIXME It's not elegant, but I didn't find elegant soluiton
+JSON2.write(io::IO, p::Plot; kwargs...) = write(io, JSON.json(p))
 end
