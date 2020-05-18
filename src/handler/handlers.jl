6d03c7747653a1b01dfd1696424c82dad9810fe5
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2020-05-17 23:18:20 -0400

    Update src/handler/handlers.jl

--- src/handler/handlers.jl
+++ src/handler/handlers.jl
@@ -168,7 +168,7 @@ validate_layout(layout::Component) = validate(layout)
 
 validate_layout(layout::Function) = validate_layout(layout())
 
-validate_layout(layout) = error("layout must be Component or function that returns Component")
+validate_layout(layout) = error("The layout must be a component, tree of components, or a function which returns a component.")
 #For test purposes, with the ability to pass a custom registry
 function make_handler(app::DashApp, registry::ResourcesRegistry; check_layout = false)
             
