f59aae9d58b45bd0d0f654f6387569bc5be48cc9
Alexandr Romanenko <waralex@gmail.com>
2021-10-06 17:50:16 +0300

    dev.jl comment

--- test/dev.jl
+++ test/dev.jl
@@ -1,3 +1,8 @@
+#=
+dev.jl is a draft file that I use during the development of a new functionality if I need to call some function, etc. and see how it works.
+I.e. at the beginning of the development of a new functionality, I disable all tests except dev.jl,
+after the overall architecture emerges, I disable dev.jl and start writing full-fledged tests
+=#
 using Test
 using Dash
 using JSON2
