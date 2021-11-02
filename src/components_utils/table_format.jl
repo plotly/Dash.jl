ed884ac78944bcdabddc45a2952a6456c9e5503f
Alexandr Romanenko <waralex@gmail.com>
2021-11-02 19:46:31 +0300

    in work

--- src/components_utils/table_format.jl
+++ src/components_utils/table_format.jl
@@ -1,5 +1,5 @@
 module TableFormat
-    export Format
+    export Format, Align, Group, Padding, Prefix, Scheme, Sign, DSymbol, Trim
 
     struct NamedValue{Name, T}
         value::T
@@ -90,75 +90,59 @@ module TableFormat
         yes = "~"
     ))
 
-    abstract type FormatProperty end;
-
-    struct NamedProperty{Name, Keys} <: FormatProperty
-        value::NamedValue{Name}
-        function NamedProperty{Name, Keys}(value::NamedValue{ValueName}) where {Name, ValueName, Keys}
-            Name != ValueName && throw(ArgumentError("expected value to be one of $(join(string.(string(Name), ".", string.(Keys)), ", "))"))
-            return new{Name, Keys}(value)
-        end
-    end
-
-    property_by_tuple(::TupleWithNamedValues{Name, Keys}) where {Name, Keys} = NamedProperty{Name, Keys}
-
-    get_value(p::NamedProperty) = p.value.value
-
-    struct CharProperty <: FormatProperty
-        value::Char
-        CharProperty(value::Char) = new(value)
-        function CharProperty(value::String)
-            length(value) != 1 && throw(ArgumentError("expected char or string of length one"))
-            return new(value[1])
-        end
-        CharProperty(value) = throw(ArgumentError("expected char or string of length one"))
-    end
-
-    get_value(p::CharProperty) = p.value
-
-    struct StringProperty <: FormatProperty
-        value::String
-        StringProperty(value::String) = new(value)
-        StringProperty(value) = throw(ArgumentError("expected value to be a string"))
-    end
-
-    get_value(p::StringProperty) = p.value
-
-    struct UIntOrNothingProperty <: FormatProperty
-        value::Union{UInt, Nothing}
-        UIntOrNothingProperty(v::Nothing) = new(v)
-        function UIntOrNothingProperty(v::Integer)
-            v < 0 && throw(ArgumentError("expected value to be non-negative"))
-            return new(v)
-        end
-        UIntOrNothingProperty(v) = throw(ArgumentError("expected value to be an integer"))
-    end
-
-    get_value(p::UIntOrNothingProperty) = p.value
-
     mutable struct Format
         locale
         nully
         prefix
         specifier
-        function Format()
-            return new(
+        function Format(;
+                align = Align.default,
+                fill = nothing,
+                group = Group.no,
+                padding = Padding.no,
+                padding_width = nothing,
+                precision = nothing,
+                scheme = Scheme.default,
+                sign = Sign.default,
+                symbol = DSymbol.no,
+                trim = Trim.no,
+
+                symbol_prefix = nothing,
+                symbol_suffix = nothing,
+                decimal_delimiter = nothing,
+                group_delimiter = nothing,
+                groups = nothing,
+
+                nully = nothing,
+
+                si_prefix = Prefix.none
+            )
+            result = new(
                 Dict(),
                 "",
-                Prefix.none,
-                Dict(
-                    :align => Align.default,
-                    :fill => "",
-                    :group => Group.no,
-                    :width => "",
-                    :padding => Padding.no,
-                    :precision => "",
-                    :sign => Sign.default,
-                    :symbol => DSymbol.no,
-                    :trim => Trim.no,
-                    :type => Scheme.default
-                )
+                Prefix.none.value,
+                Dict{Symbol, Any}()
             )
+            align!(result, align)
+            fill!(result, fill)
+            group!(result, group)
+            padding!(result, padding)
+            padding_width!(result, padding_width)
+            precision!(result, precision)
+            scheme!(result, scheme)
+            sign!(result, sign)
+            symbol!(result, symbol)
+            trim!(result, trim)
+
+            !isnothing(symbol_prefix) && symbol_prefix!(result, symbol_prefix)
+            !isnothing(symbol_suffix) && symbol_suffix!(result, symbol_suffix)
+            !isnothing(decimal_delimiter) && decimal_delimiter!(result, decimal_delimiter)
+            !isnothing(group_delimiter) && group_delimiter!(result, group_delimiter)
+            !isnothing(groups) && groups!(result, groups)
+            !isnothing(nully) && nully!(result, nully)
+            !isnothing(si_prefix) && si_prefix!(result, si_prefix)
+
+            return result
         end
     end
 
@@ -170,10 +154,10 @@ module TableFormat
         throw(ArgumentError("expected value to be one of $(possible_values(t))"))
     end
 
-    check_char_value(v::Char) = v
+    check_char_value(v::Char) = string(v)
     function check_char_value(v::String)
-        length(v) != 1 && throw(ArgumentError("expected char or string of length one"))
-        return v[1]
+        length(v) > 1 && throw(ArgumentError("expected char or string of length one"))
+        return v
     end
     function check_char_value(v)
         throw(ArgumentError("expected char or string of length one"))
@@ -189,10 +173,14 @@ module TableFormat
 
     function align!(f::Format, value)
         check_named_value(Align, value)
-        f.specifier[:align] = value
+        f.specifier[:align] = value.value
     end
 
     function fill!(f::Format, value)
+        if isnothing(value)
+            f.specifier[:fill] = ""
+            return
+        end
         v = check_char_value(value)
         f.specifier[:fill] = v
     end
@@ -203,7 +191,7 @@ module TableFormat
             value = value ? Group.yes : Group.no
         end
         check_named_value(Group, value)
-        f.specifier[:group] = value
+        f.specifier[:group] = value.value
     end
 
     function padding!(f::Format, value)
@@ -211,7 +199,7 @@ module TableFormat
             value = value ? Padding.yes : Padding.no
         end
         check_named_value(Padding, value)
-        f.specifier[:padding] = value
+        f.specifier[:padding] = value.value
     end
 
     function padding_width!(f::Format, value)
@@ -234,17 +222,17 @@ module TableFormat
 
     function scheme!(f::Format, value)
         check_named_value(Scheme, value)
-        f.specifier[:type] = value
+        f.specifier[:type] = value.value
     end
 
     function sign!(f::Format, value)
         check_named_value(Sign, value)
-        f.specifier[:sign] = value
+        f.specifier[:sign] = value.value
     end
 
     function symbol!(f::Format, value)
         check_named_value(DSymbol, value)
-        f.specifier[:symbol] = value
+        f.specifier[:symbol] = value.value
     end
 
     function trim!(f::Format, value)
@@ -252,12 +240,12 @@ module TableFormat
             value = value ? Trim.yes : Trim.no
         end
         check_named_value(Trim, value)
-        f.specifier[:trim] = value
+        f.specifier[:trim] = value.value
     end
 
     # Locale
     function symbol_prefix!(f::Format, value::AbstractString)
-        if haskey(f.locale, :symbol)
+        if !haskey(f.locale, :symbol)
             f.locale[:symbol] = [value, ""]
         else
             f.locale[:symbol][1] = value
@@ -265,7 +253,7 @@ module TableFormat
     end
 
     function symbol_suffix!(f::Format, value::AbstractString)
-        if haskey(f.locale, :symbol)
+        if !haskey(f.locale, :symbol)
             f.locale[:symbol] = ["", value]
         else
             f.locale[:symbol][2] = value
@@ -281,6 +269,54 @@ module TableFormat
         v = check_char_value(value)
         f.locale[:group] = v
     end
+
+    function groups!(f::Format, value::Union{Vector{<:Integer}, <:Integer})
+        groups = value isa Integer ? [value] : value
+        isempty(groups) && throw(ArgumentError("groups cannot be empty"))
+
+        for g in groups
+            g < 0 && throw(ArgumentError("group entry must be non-negative integer"))
+        end
+        f.locale[:grouping] = groups
+    end
+
+    # Nully
+    function nully!(f::Format, value)
+        f.nully = value
+    end
+
+    # Prefix
+    function si_prefix!(f::Format, value)
+        check_named_value(Prefix, value)
+        f.prefix = value.value
+    end
+
+    JSON3.StructTypes.StructType(::Type{Format}) = JSON3.RawType()
+
+    function JSON3.rawbytes(f::Format)
+        aligned = f.specifier[:align] != Align.default.value
+        fill = aligned ? f.specifier[:fill] : ""
+        spec_io = IOBuffer()
+        print(spec_io,
+            aligned ? f.specifier[:fill] : "",
+            f.specifier[:align],
+            f.specifier[:sign],
+            f.specifier[:symbol],
+            f.specifier[:padding],
+            f.specifier[:width],
+            f.specifier[:group],
+            f.specifier[:precision],
+            f.specifier[:trim],
+            f.specifier[:type]
+        )
+        return JSON3.write(
+            locale = deepcopy(f.locale),
+            nully = f.nully,
+            prefix = f.prefix,
+            specifier = String(take!(spec_io))
+        )
+    end
+
 
     #=struct Format
         align ::Union{typeof(Align), Nothing}
@@ -567,7 +603,6 @@ class Format:
     # Prefix
     def si_prefix(self, value):
         self._validate_named(value, Prefix)
-
         self._prefix = value
         return self
 
--- test/table_format.jl
+++ test/table_format.jl
@@ -13,30 +13,33 @@ end
 @testset "specifier" begin
     f = Format()
 
-    @test f.specifier[:align] == TableFormat.Align.default
+    @test f.specifier[:align] == TableFormat.Align.default.value
     TableFormat.align!(f, TableFormat.Align.left)
-    @test f.specifier[:align] == TableFormat.Align.left
+    @test f.specifier[:align] == TableFormat.Align.left.value
     @test_throws ArgumentError TableFormat.align!(f, TableFormat.Group.no)
 
     TableFormat.fill!(f, '-')
-    @test f.specifier[:fill] == '-'
+    @test f.specifier[:fill] == "-"
+    TableFormat.fill!(f, "+")
+    @test f.specifier[:fill] == "+"
+    @test_throws ArgumentError TableFormat.fill!(f, "++")
 
 
     TableFormat.group!(f, TableFormat.Group.yes)
-    @test f.specifier[:group] == TableFormat.Group.yes
+    @test f.specifier[:group] == TableFormat.Group.yes.value
 
     TableFormat.group!(f, true)
-    @test f.specifier[:group] == TableFormat.Group.yes
+    @test f.specifier[:group] == TableFormat.Group.yes.value
     TableFormat.group!(f, false)
-    @test f.specifier[:group] == TableFormat.Group.no
+    @test f.specifier[:group] == TableFormat.Group.no.value
     @test_throws ArgumentError TableFormat.group!(f, "ffff")
 
     TableFormat.padding!(f, TableFormat.Padding.yes)
-    @test f.specifier[:padding] == TableFormat.Padding.yes
+    @test f.specifier[:padding] == TableFormat.Padding.yes.value
     TableFormat.padding!(f, true)
-    @test f.specifier[:padding] == TableFormat.Padding.yes
+    @test f.specifier[:padding] == TableFormat.Padding.yes.value
     TableFormat.padding!(f, false)
-    @test f.specifier[:padding] == TableFormat.Padding.no
+    @test f.specifier[:padding] == TableFormat.Padding.no.value
     @test_throws ArgumentError TableFormat.padding!(f, "ffff")
 
     TableFormat.padding_width!(f, 100)
@@ -53,32 +56,120 @@ end
     @test_throws ArgumentError TableFormat.precision!(f, "ffff")
     @test_throws ArgumentError TableFormat.precision!(f, -10)
 
-    @test f.specifier[:type] == TableFormat.Scheme.default
+    @test f.specifier[:type] == TableFormat.Scheme.default.value
     TableFormat.scheme!(f, TableFormat.Scheme.decimal)
-    @test f.specifier[:type] == TableFormat.Scheme.decimal
+    @test f.specifier[:type] == TableFormat.Scheme.decimal.value
     @test_throws ArgumentError TableFormat.scheme!(f, "ccc")
 
-    @test f.specifier[:sign] == TableFormat.Sign.default
+    @test f.specifier[:sign] == TableFormat.Sign.default.value
     TableFormat.sign!(f, TableFormat.Sign.negative)
-    @test f.specifier[:sign] == TableFormat.Sign.negative
+    @test f.specifier[:sign] == TableFormat.Sign.negative.value
     @test_throws ArgumentError TableFormat.sign!(f, "ccc")
 
-    @test f.specifier[:symbol] == TableFormat.DSymbol.no
+    @test f.specifier[:symbol] == TableFormat.DSymbol.no.value
     TableFormat.symbol!(f, TableFormat.DSymbol.yes)
-    @test f.specifier[:symbol] == TableFormat.DSymbol.yes
+    @test f.specifier[:symbol] == TableFormat.DSymbol.yes.value
     @test_throws ArgumentError TableFormat.symbol!(f, "ccc")
 
-    @test f.specifier[:trim] == TableFormat.Trim.no
+    @test f.specifier[:trim] == TableFormat.Trim.no.value
     TableFormat.trim!(f, TableFormat.Trim.yes)
-    @test f.specifier[:trim] == TableFormat.Trim.yes
+    @test f.specifier[:trim] == TableFormat.Trim.yes.value
     TableFormat.trim!(f, false)
-    @test f.specifier[:trim] == TableFormat.Trim.no
+    @test f.specifier[:trim] == TableFormat.Trim.no.value
     @test_throws ArgumentError TableFormat.trim!(f, "ccc")
 
 end
 
 @testset "locale" begin
     f = Format()
+    @test isempty(f.locale)
+    TableFormat.symbol_prefix!(f, "lll")
+    @test haskey(f.locale, :symbol)
+    @test f.locale[:symbol][1] == "lll"
+    @test f.locale[:symbol][2] == ""
+    TableFormat.symbol_prefix!(f, "rrr")
+    @test f.locale[:symbol][1] == "rrr"
+    @test f.locale[:symbol][2] == ""
 
-    
+
+    f = Format()
+    @test isempty(f.locale)
+    TableFormat.symbol_suffix!(f, "lll")
+    @test haskey(f.locale, :symbol)
+    @test f.locale[:symbol][1] == ""
+    @test f.locale[:symbol][2] == "lll"
+    TableFormat.symbol_suffix!(f, "rrr")
+    @test f.locale[:symbol][1] == ""
+    @test f.locale[:symbol][2] == "rrr"
+
+    TableFormat.symbol_prefix!(f, "kkk")
+    @test f.locale[:symbol][1] == "kkk"
+    @test f.locale[:symbol][2] == "rrr"
+
+    f = Format()
+    TableFormat.decimal_delimiter!(f, '|')
+    @test f.locale[:decimal] == "|"
+    TableFormat.group_delimiter!(f, ';')
+    @test f.locale[:group] == ";"
+
+    TableFormat.groups!(f, [3,3,3])
+    @test f.locale[:grouping] == [3,3,3]
+
+    TableFormat.groups!(f, [5])
+    @test f.locale[:grouping] == [5]
+
+    @test_throws ArgumentError TableFormat.groups!(f, Int[])
+    @test_throws ArgumentError TableFormat.groups!(f, Int[-1,2])
+end
+
+@testset "nully & prefix" begin
+    f = Format()
+    TableFormat.nully!(f, "gggg")
+    @test f.nully == "gggg"
+    TableFormat.si_prefix!(f, TableFormat.Prefix.femto)
+    @test f.prefix == TableFormat.Prefix.femto.value
+end
+
+@testset "Format creation" begin
+    f = Format(
+        align = Align.left,
+        fill = "+",
+        group = true,
+        padding = Padding.yes,
+        padding_width = 10,
+        precision = 2,
+        scheme = Scheme.decimal,
+        sign = Sign.negative,
+        symbol = DSymbol.yes,
+        trim = Trim.yes,
+
+        symbol_prefix = "tt",
+        symbol_suffix = "kk",
+        decimal_delimiter = ";",
+        group_delimiter = ",",
+        groups = [2, 2],
+        nully = "f",
+        si_prefix = Prefix.femto
+    )
+
+    @test f.specifier == Dict{Symbol, Any}(
+        :align => Align.left.value,
+        :fill => "+",
+        :group => Group.yes.value,
+        :padding => Padding.yes.value,
+        :width => 10,
+        :precision => ".2",
+        :type => Scheme.decimal.value,
+        :sign => Sign.negative.value,
+        :symbol => DSymbol.yes.value,
+        :trim => Trim.yes.value
+    )
+
+    @test f.nully == "f"
+    @test f.prefix == Prefix.femto.value
+
+    @test f.locale[:symbol] == ["tt", "kk"]
+    @test f.locale[:decimal] == ";"
+    @test f.locale[:group] == ","
+    @test f.locale[:grouping] == [2,2]
 end
