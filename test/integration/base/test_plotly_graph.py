14b3eb44905081ebb02fb0d5155753a3963ac14c
Alexandr Romanenko <waralex@gmail.com>
2022-02-24 11:25:21 +0300

    remove PlotlyJS test

--- test/integration/base/test_plotly_graph.py
+++ test/integration/base/test_plotly_graph.py
@@ -25,19 +25,3 @@ def test_jlpg001_plotly_graph(dashjl):
     dashjl.wait_for_text_to_equal("#status", "second", timeout=10)
 
     dashjl.percy_snapshot(name="PlotlyBase figure callback")
-
-def test_jlpg002_plotlyjs_graph(dashjl):
-    fp = jl_test_file_path("jlpg002_plotlyjs_graph.jl")
-    dashjl.start_server(fp)
-    dashjl.wait_for_element_by_css_selector(
-        "#graph", timeout=120
-    )
-
-    dashjl.wait_for_text_to_equal("#status", "first", timeout=10)
-
-    dashjl.percy_snapshot(name="PlotlyJS figure layout")
-
-    dashjl.find_element("#draw").click()
-    dashjl.wait_for_text_to_equal("#status", "second", timeout=10)
-
-    dashjl.percy_snapshot(name="PlotlyJS figure callback")
